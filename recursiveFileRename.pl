34c6002b5a09841e88939351ecaaa22790ca6d76
mattgrobelny <mattgrobelny@gmail.com>
2016-06-28 16:00:33 -0500

    Fixed bugs

--- recursiveFileRename.pl
+++ recursiveFileRename.pl
@@ -1,7 +1,6 @@
 use warnings;
-use Getopt::Long;
 
-if (($AVRG[1] eq "-h")||($AVRG[1] eq "--help")){
+if (($ARGV[0] eq "-h")||($ARGV[0] eq "--help")){
   print(
   " File Renaming Script\n\n
   This script will renames all files in a directory, starting from first to last, to:\n
@@ -9,7 +8,7 @@ if (($AVRG[1] eq "-h")||($AVRG[1] eq "--help")){
   \n
 
   All parameters and defaults are:\n
-  Defaults= []\n\n
+  Defaults= []\n
 
   filename= [No default require]-- Name of file\n
   filemin='0' -- Lowest file number\n
@@ -24,8 +23,10 @@ if (($AVRG[1] eq "-h")||($AVRG[1] eq "--help")){
   die;
 };
 
+use Getopt::Long;
 # Get current directory
 my $current_dir= `pwd`;
+chomp($current_dir)
 
 #collect file names in directory
 my @filenames_in_dir = `ls`;
@@ -44,16 +45,18 @@ my $verbose='FAlSE';
   'filename=s' => \$filename,
   'filemin:i' => \$filemin,
   'filemax:i'=> \$filemax,
-  '$file_ext:s'=> \$file_ext,
+  'file_ext:s'=> \$file_ext,
   'v:s'=> \$verbose
 );
 
+for ($filemin_start = $filemin ;  $filemin_start <= $filemax; $filemin_start=$filemin_start+1) {
+  my $it_filename= $filename . $filemin_start . $file_ext;
+   my $file_to_change=chomp($filenames_in_dir[$filemin_start]);
+  if ($verbose eq "TRUE") {
+    print("Now renaming $filenames_in_dir[$filemin_start] to $it_filename \n");
+    system("mv $current_dir/$file_to_change $current_dir/$it_filename");
+  }
+  system("mv $current_dir/$file_to_change $current_dir/$it_filename");
+}
 
-# for ($filemin_start = $filemin ;  $filemin_start <= $filemax; $filemin_start=$filemin_start++) {
-#   my $it_filename= $filename . $additional_text . $filemin_start;
-#   if ($verbose eq "TRUE") {
-#     print("Now renaming $filenames_in_dir[$filemin_start] to $it_filename");
-#     system("rename $it_filename * $filenames_in_dir[$filemin_start]" );
-#   }
-#   system("rename $it_filename * $filenames_in_dir[$filemin_start]" );
-# }
+die;
